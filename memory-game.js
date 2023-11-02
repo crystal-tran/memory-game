8de4e5a86b7ee7b72fae2ffe5a8967e543cb4964
Crystal Tran <c.t.tran95@gmail.com>
2023-11-02 11:46:22 -0400

    Added comments and transition

--- memory-game.js
+++ memory-game.js
@@ -1,3 +1,9 @@
+
+//function is called when the window loads to create a smooth transition
+//querySelector searches for the first element with the class of transition
+//removes the 'is-active' class from that element after a 500ms delay
+//makes the overlay invisible
+
 window.onload = function() {
   const transition = document.querySelector('.transition');
   setTimeout(() => {
@@ -47,74 +53,101 @@ function shuffle(items) {
   - a click event listener for each card to handleCardClick
  */
 
-
+  //function to create memory cards with the shuffled color array as a parameter
 function createCards(colors) {
+  //retrieves the 'game' element by its ID, this will be the container of the memory card
   const gameBoard = document.getElementById("game");
 
+  // iterates over each color in the array
   for (let color of colors) {
     // missing code here ...
+
+    //creates a new div element, which will be the card
     const newDiv = document.createElement("div");
+
+    //adds a color class to the div element
     newDiv.classList.add(color);
+
+    //a click event listener is added to the card and it calls back the function handleCardClick
     newDiv.addEventListener("click", handleCardClick)
+
+    //appends the newly created card to the gameBoard container
     gameBoard.appendChild(newDiv);
   }
 }
 
 /** Flip a card face-up. */
-
+//function sets the background color of the flipped card based on its class
+//and adds the class of flipped
 function flipCard(card) {
   card.style.backgroundColor = card.classList[0];
   card.classList.add("flipped");
 }
 
 /** Flip a card face-down. */
-
+//function resets the background color and removes the class of flipped
 function unFlipCard(card) {
   card.style.backgroundColor = "";
   card.classList.remove("flipped");
 }
 
-/** Handle clicking on a card: this could be first-card or second-card. */
-let lock = false
+//variable to control whether clicking is enabled
+let disableClick = false
+//variable to keep track of the number of flipped cards
 let cardCount = 0;
+//an array to store the two flipped cards
 let flippedCards = [];
+//variable to keep track of score
 let numTries = 0;
 let score = document.getElementById("score");
 score.style.display = "inline"
 
+/* Handle clicking on a card: this could be first-card or second-card. */
 function handleCardClick(e) {
-  if(lock){
+  //checks if clicking on cards is disabled, if true then function does not proceed
+  if(disableClick){
     return;
   }
 
+
   let card = e.target;
 
+  //if the card does not contain the class of "flipped" and there are <= 2 flipped cards
+  //call the function flipCard
+  //push that card into flippedCards array
   if(!card.classList.contains("flipped") && flippedCards.length <= 2){
     flipCard(card);
     flippedCards.push(card);
   }
 
+  //if two cards are flipped, increase score count and disable clicking
   if(flippedCards.length === 2){
-    lock = true;
+    disableClick = true;
     numTries++;
     score.innerText = numTries;
 
+    //if the flipped cards match in color, remove the click event listener and increase cardCount
+    //clear out flippedCards array and enable clicking
     if(flippedCards[0].style.backgroundColor === flippedCards[1].style.backgroundColor){
       flippedCards[0].removeEventListener("click", handleCardClick);
       flippedCards[1].removeEventListener("click", handleCardClick);
       flippedCards = [];
-      lock = false;
+      disableClick = false;
       cardCount += 2;
     } else {
+
+      //if the flipped cards did not match, call the function unFlipCard after 1000ms
+      //clear out the flippedCards array and enable clicking
       setTimeout(function (){
         unFlipCard(flippedCards[0]);
         unFlipCard(flippedCards[1]);
         flippedCards = [];
-        lock = false;
+        disableClick = false;
       }, FOUND_MATCH_WAIT_MSECS);
     }
   }
 
+  //if all cards have been matched, alert the user their score
   if(cardCount === COLORS.length){
     setTimeout(function(){
       alert("You found them all in " + numTries + " tries!")
